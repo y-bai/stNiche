a66cdc1b5aa29e3453d0f428cd25b3aaa7732a50
Yong Bai <yong.bai@hotmail.com>
2023-07-07 12:02:45 +0800

    update

---
+++ stNiche/train/_commu_trainer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author: Yong Bai, yong.bai@hotmail.com
+@Time: 2023/7/6 17:57
+@License: (C) Copyright 2013-2023. 
+@File: _commu_trainer.py
+@Desc:
+
+"""
